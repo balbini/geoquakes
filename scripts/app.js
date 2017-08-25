8295f77d48fef86db65928f725198d39581778b1
balbini <balbini91@gmail.com>
2017-08-24 19:24:31 -0700

    printing earthquake titles

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,23 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 
 $(document).ready(function() {
   console.log("Let's get coding!");
-  // CODE IN HERE!
+
+$.ajax({
+  type: "GET",
+  url: weekly_quakes_endpoint
+})
+.then(function(earthquake){
+for (i = 0; i < earthquake.features.length; i++){
+  let shake = earthquake.features[i].properties.title;
+  console.log(shake);
+}
+})
+.catch(function(err){
+  console.log(err);
+});
+
+function update(){
+  $(".rolling-shake").append(`<p>${shake[i]}</p>`);
+}
 
 });
